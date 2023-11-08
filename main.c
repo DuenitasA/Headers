0e8bbbd937ccf487f518f550a2ad060c0618f5ab
Alejandro Duenas <alejandro.duenas@udla.edu.ec>
2023-11-08 15:06:33 -0500

    Creamos el archivo de cabecera con la funcion de suma

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+    int a, b;
+
+    printf("Ingrese el primer digito.\n");
+    scanf("%d", &a);
+    printf("Ingrese el segundo digito.\n");
+    scanf("%d", &b);
+
+    return 0;
+}
